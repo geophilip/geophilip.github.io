14b8b176c67a59bc1adde1d8b609031acc110f27
George Philip <155936991+geophilip@users.noreply.github.com>
2024-08-23 19:53:05 -0500

    Delete CS396-PROJ directory

--- CS396-PROJ/javascript/contactus_js.js
+++
@@ -1,9 +0,0 @@
- document.addEventListener("DOMContentLoaded", () => {
-      anime({
-        targets: '.team-member',
-        opacity: [0, 1],
-        translateY: [-50, 0],
-        duration: 1000,
-        delay: anime.stagger(200) 
-      });
-    });
